2349a010c8f58f8db00565e7c823d08aae1df679
Ralf S. Engelschall <rse@engelschall.com>
2017-04-10 20:42:21 +0200

    get working under newer ESlint world order

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function (grunt) {
         },
         eslint: {
             options: {
-                config: "eslint.json"
+                configFile: "eslint.json"
             },
             target: [ "tasks/grunt-bower-install-simple.js" ]
         },
